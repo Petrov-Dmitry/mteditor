36957f1f659fe6dc389351c525e8a660daaa06c6
Petrov Dmitry (aka Holger Dee Assuran) <holger.dee.assuran@gmail.com>
2014-06-24 12:40:34 +0400

    Формирование меню

--- js/jquery.mteditor.js
+++ js/jquery.mteditor.js
@@ -15,6 +15,7 @@
             },
             textTypes:{
                 headers:{
+                    plain:'Plain text',
                     h1:'Header 1',
                     h2:'Header 2',
                     h3:'Header 3',
@@ -44,6 +45,9 @@
             others:{
                 link:'Insert link',
                 image:'Insert image'
+            },
+            clear:{
+                clear:'Clear formatting'
             }
         };
         //console.log('MTeditor function-buttons', fnMenu);
@@ -54,8 +58,8 @@
         var wrapper = '<div class="mteditor"></div>',
             menuWrapper = '<div class="menu"></div>',
             menuGroup = '<div class="group"></div>',
-            menuMode = '<select name="activeMode"></select>',
-            menuModeOption = '<option value=""></option>',
+            menuSel = '<select></select>',
+            menuSelOption = '<option></option>',
             menuButton = '<div class="button"></div>',
             content = '<div class="content"></div>';
         //console.log('MTeditor wrapper block', wrapper);
@@ -83,27 +87,87 @@
          * Plugin functions
          */
         var draw = function(block) {
-            console.log('\nProceeding block', block);
+            console.log('\nRe-draw block', block);
 
             var $block = $(block),
                 blockId = $block.attr('id'),
-                divider = options.menuDivider;
-            console.log('blockId', blockId);
-            console.log('Menu buttons', fnMenu);
-            console.log('Menu divider', divider);
+                divider = options.menuDivider
+                text = $block.html();
+            //console.log('blockId', blockId);
+            //console.log('Menu buttons', fnMenu);
+            //console.log('Menu divider', divider);
+            //console.log('Editable text', text);
 
             // Make the MTeditor-block
             wrapper = $(wrapper).attr('id', blockId);
-            console.log('wrapper', wrapper);
+            //console.log('wrapper', wrapper);
             if (options.resizable === true) {
                 wrapper.addClass('resizable').resizable();
             }
 
-            // Add the menu-block into MTeditor
-            wrapper.append(menuWrapper).append($(content).attr('contentEditable', true));
+            // Build the MTeditor menu
+            var i = 0,
+                menu = $(menuWrapper);
+            $.each(fnMenu, function(index, buttons) {
+                //console.log(index, buttons);
+                // Create group
+                if (index !== 'editorMode') {
+                    group = $(menuGroup).attr('id', index);
+                    $.each(buttons, function(key, value) {
+                        //console.log('Button', key, value);
+                        // Create buttons
+                        if (key !== 'headers') {
+                            button = $(menuButton).html(value).attr('id', key).attr('title', value);
+                            //console.log('Button ready', button);
+                            // Insert button into the group
+                            group.append(button);
+                        // Create heders select
+                        } else {
+                            select = $(menuSel).attr('name', key);
+                            //console.log('heders select', select);
+                            $.each(value, function(opt, val) {
+                                //console.log('Option', opt, val);
+                                button = $(menuSelOption).attr('id', opt).val(opt).html(val);
+                                // Insert option into the select
+                                select.append(button);
+                            });
+                            // Insert select into the group
+                            group.append(select);
+                        }
+                    });
+                    //console.log('Group ready', group);
+                // editorMode group
+                } else {
+                    group = $(menuGroup).attr('id', index);
+                    select = $(menuSel).attr('name', index);
+                    // Create select
+                    $.each(buttons, function(key, value) {
+                        //console.log('Button', key, value);
+                        button = $(menuSelOption).attr('id', key).val(key).html(value);
+                        //console.log('Button ready', button);
+                        // Insert option into the select
+                        select.append(button);
+                        // Insert select into the group
+                        group.append(select);
+                    });
+                }
+                //console.log(group);
+                // Insert group into the menu
+                if (i > 0) {
+                    menu.append(divider);
+                }
+                menu.append(group);
+                i++;
+            });
+            //console.log('menu', menu);
+
+            // Add the menu and content-block into MTeditor
+            wrapper.append(menu).append($(content).attr('contentEditable', true).html(text));
 
             // Hide TEXTAREA and draw MTeditor-block
             $block.hide().before(wrapper);
+
+            return wrapper;
         }
 
         /*
@@ -113,7 +177,8 @@
             //console.log('Founded block', this);
             var $this = $(this);
             //console.log('$this', $this);
-            draw(this);
+            block = draw(this);
+            console.log('block', block);
         });
     };
 
